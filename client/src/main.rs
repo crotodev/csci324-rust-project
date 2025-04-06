e7a11cf4ae3522340bbefafb74fa9182d4f58c29
Christian Rotondo <chris.rotondo6@gmail.com>
2025-04-06 19:39:02 -0400

    Added logic to main.rs.

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,3 +1,48 @@
+use std::env;
+
+mod producer;
+mod consumer;
+mod message;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        eprintln!("Usage: {} <command>", args[0]);
+        std::process::exit(1);
+    }
+
+    let command: &String = &args[1];
+    match command.as_str() {
+        "produce" => {
+            if args.len() < 5 {
+                eprintln!("Usage: {} <address> <topic> <n>", args[0]);
+                std::process::exit(1);
+            }
+            let address: &String = &args[2];
+            let topic: &String = &args[3];
+            let n: &i32 = &args[4].parse::<i32>().unwrap();
+
+            let mut p: producer::Producer = producer::Producer::new(address).unwrap();
+            
+            for _ in 0..*n {
+                if let Err(e) = p.send(topic, "Hello, world!") {
+                    eprintln!("Failed to send message: {}", e);
+                }
+            }
+        }
+        "consume" => {
+            if args.len() < 4 {
+                eprintln!("Usage: {} <address> <topic>", args[0]);
+                std::process::exit(1);
+            }
+
+            let address: &String = &args[2];
+            let topic: &String = &args[3];
+            
+        }
+        _ => {
+            eprintln!("Unknown command: {}", command);
+            std::process::exit(1);
+        }
+    }
 }
