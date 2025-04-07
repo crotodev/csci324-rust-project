9c265abd9c38b31c7165c4589d0a973ea3789d59
Christian Rotondo <chris.rotondo6@gmail.com>
2025-04-07 19:42:22 -0400

    Added documentation for Producer.

--- client/src/producer.rs
+++ client/src/producer.rs
@@ -5,19 +5,22 @@ use std::time::Duration;
 
 type Result<T> = std::result::Result<T, Box<dyn Error + Send + Sync>>;
 
+// This struct represents a generic Producer that can send messages to a server.
 pub struct Producer {
     stream: TcpStream,
 }
 
 impl Producer {
     pub fn new(address: &str) -> Result<Self> {
-        let stream = TcpStream::connect_timeout(&address.parse()?, Duration::from_secs(5))?;
+        let stream: TcpStream =
+            TcpStream::connect_timeout(&address.parse()?, Duration::from_secs(5))?;
         stream.set_nodelay(true)?;
         Ok(Producer { stream })
     }
 
+    /// Sends a message to the specified topic
     pub fn send(&mut self, topic: &str, message: &str) -> Result<()> {
-        let command = format!("PUBLISH {} {}\n", topic, message);
+        let command: String = format!("PUBLISH {} {}\n", topic, message);
         self.stream.write_all(command.as_bytes())?;
         self.stream.flush()?;
         Ok(())
